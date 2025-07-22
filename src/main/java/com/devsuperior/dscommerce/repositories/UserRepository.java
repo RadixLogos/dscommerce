c11c135fedfddec779616051f80ccb0b3820da7b
rubens <rubenshds@gmail.com>
2025-07-22 19:03:45 -0300

    import.sql + corrections

--- src/main/java/com/devsuperior/dscommerce/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/dscommerce/repositories/UserRepository.java
@@ -1,15 +1,18 @@
 package com.devsuperior.dscommerce.repositories;
 
+import com.devsuperior.dscommerce.entities.User;
 import com.devsuperior.dscommerce.projections.UserDetailsProjection;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
-public interface UserRepository {
-    @Query(nativeQuery = true, value = "SELECT tb_user.email AS USERNAME, tb_role.id AS ROLE_ID, tb.authority " +
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    @Query(nativeQuery = true, value = "SELECT tb_user.email AS USERNAME, tb_roles.id AS ROLE_ID, tb_roles.authority " +
             "FROM tb_user " +
             "INNER JOIN tb_user_role ON tb_user.id = tb_user_role.user_id " +
-            "INNER JOIN tb_role ON tb_user_role.role_id = tb_role.id " +
+            "INNER JOIN tb_roles ON tb_user_role.role_id = tb_roles.id " +
             "WHERE tb_user.email = :email")
     List<UserDetailsProjection> getUserByEmailWithRole(String email);
 
