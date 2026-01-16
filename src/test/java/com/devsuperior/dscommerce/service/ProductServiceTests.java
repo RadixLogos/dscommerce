3286fae1703e07941a3d2454772a738ea85e4ab4
rubens <rubenshds@gmail.com>
2026-01-16 10:58:49 -0300

    ProductServiceTests findAllProducts

--- src/test/java/com/devsuperior/dscommerce/service/ProductServiceTests.java
+++ src/test/java/com/devsuperior/dscommerce/service/ProductServiceTests.java
@@ -11,10 +11,17 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
@@ -26,15 +33,23 @@ public class ProductServiceTests {
 
     private Long existingId;
     private Long unexistingId;
+    private String existingProductName;
+    private String unexistingProductName;
     private Product product;
+    private List<Product> products;
     @BeforeEach
     void setup(){
         existingId=1L;
         unexistingId=2L;
+        existingProductName="Luz sobre a idade média";
+        unexistingProductName = "Pc gamer";
         product = Factory.buildProduct();
-
+        products = new ArrayList<>();
+        products.add(product);
+        Page<Product> pagedProducts = new PageImpl<>(products, Pageable.ofSize(12),products.size());
         when(productRepository.findById(existingId)).thenReturn(Optional.of(product));
         when(productRepository.findById(unexistingId)).thenReturn(Optional.empty()).thenThrow(ResourceNotFoundException.class);
+        when(productRepository.findByName(eq(existingProductName),any())).thenReturn(pagedProducts);
     }
 
    @Test
@@ -52,5 +67,14 @@ public class ProductServiceTests {
         Assertions.assertThrows(ResourceNotFoundException.class,()->{
             productService.findProductById(unexistingId);
         });
+    }
+
+    @Test
+    public void findAllProductsShouldReturnPageProductDTOWhenExistingName(){
+        var response = productService.findAllProducts(Pageable.unpaged(),existingProductName);
+        Assertions.assertEquals(products.size(),response.getTotalElements());
+        Assertions.assertEquals(products.getFirst().getId(),response.getContent().getFirst().id());
+        Assertions.assertEquals(products.getFirst().getPrice(),response.getContent().getFirst().price());
+        Assertions.assertEquals(products.getFirst().getImgUrl(),response.getContent().getFirst().imgUrl());
     }
 }
